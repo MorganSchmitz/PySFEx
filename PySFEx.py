0c1ad01e65040a17d3d69d157c6127054e6ea488
MorganSchmitz <morgan.schmitz@cea.fr>
2017-02-16 14:31:02 +0100

    add dummy sextractable image generator

--- PySFEx.py
+++ PySFEx.py
@@ -1,5 +1,6 @@
 import numpy as np
 from astropy.io import fits
+import copy as cp
 
 def interpsfex(dotpsfpath, x, y):
     """Use PSFEx generated model to perform spatial PSF interpolation.
@@ -18,7 +19,7 @@ def interpsfex(dotpsfpath, x, y):
         Returns
         -------
         PSF : ndarray
-            PSF postage stamp.
+            PSF imagette.
         """
     # read PSF model and extract basis and polynomial degree and scale position
     PSF_model = fits.open(dotpsfpath)[1]
@@ -45,7 +46,39 @@ def interpsfex(dotpsfpath, x, y):
     return PSF
         
         
-        
+def create_seximage(PSF_stack, fov, filename='im.fits', size=[10000,10000], noise_level=.001):
+    # determine field of view
+    posmin, posmax = np.min(fov,axis=0), np.max(fov,axis=0)
+    span = posmax-posmin
+    posmin -= .1*span
+    posmax += .1*span
+    span *= 1.2
+    # initialize image with gaussian noise
+    size = np.array([size[1],size[0]])
+    im = np.random.normal(scale=noise_level,size=size)
+    # determine central pixel positions
+    pixel_pos = ((fov-posmin)/span * size).astype(int)
+    shap = PSF_stack.shape
+    if len(shap) == 2:
+        PSFs = PSF_stack.reshape(shap[0], int(shap[1]**.5), int(shap[1]**.5))
+    else:
+        PSFs = cp.copy(PSF_stack)
+    half_size = int(PSFs.shape[1]/2)
+    for psf, pos in zip(PSFs,pixel_pos):
+        im[size[0]-pos[1]-half_size:size[0]
+-pos[1]+half_size+1, 
+           pos[0]-half_size:pos[0]+half_size+1] = psf
+    hdu = fits.PrimaryHDU(im)
+    hdu.writeto(filename, clobber=True)
+    return im
+    
+    
+    
+    
+    
+    
+    
+    
         
         
         
